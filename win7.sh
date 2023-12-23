9c54cffe8d5bb6dc2bd4a7bc7f87848bd4a01e31
ubuntu2310fake <152691685+ubuntu2310fake@users.noreply.github.com>
2023-12-23 15:22:47 +0700

    Update win7.sh

--- win7.sh
+++ win7.sh
@@ -6,8 +6,9 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3388 &>/dev/null &
+nohup ./ngrok tcp 3388 --region ap &>/dev/null &
 echo Downloading File From toigamo.blogspot.com
+sudo apt update -y
 sudo apt-get install qemu-kvm -y
 sudo apt -y install novnc python3-websockify python3-numpy
 websockify -D --web=/usr/share/novnc/ 6080 localhost:5900
@@ -22,4 +23,4 @@ echo "Chờ 30 giây để hoàn tất - Wait 30s to finish bot"
 echo "Vui lòng không tắt cửa sổ này - Dont Close This Tab"
 echo "Please support toigamo.blogspot.com thank you"
 sudo chmod 666 /dev/kvm
-sudo qemu-system-x86_64 -M q35,usb=on -device usb-tablet -cpu host -smp cores=3 -m 12G -device virtio-balloon-pci -vga qxl -device e1000e,netdev=n0 -netdev user,id=n0,hostfwd=tcp::3388-:3389 -boot c -device virtio-serial-pci -device virtio-rng-pci -enable-kvm -drive file=/tmp/w7x64.img,format=raw -drive file=driver.iso,media=cdrom  -vnc :0
+sudo qemu-system-x86_64 -M q35,usb=on -device usb-tablet -cpu host -smp cores=3 -m 12G -vga qxl -device e1000e,netdev=n0 -netdev user,id=n0,hostfwd=tcp::3388-:3389 -boot c -enable-kvm -drive file=/tmp/w7x64.img -drive file=driver.iso,media=cdrom  -vnc :0 -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0
